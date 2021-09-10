8d3cdcd037772d9d3b3d951164b000ed84ac5719
hideq <hs.show3@gmail.com>
2021-09-10 19:53:55 +0900

    first commit

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { useDispatch } from 'react-redux'
 import { addTask } from '../modules/tasksModule'
+import styled from 'styled-components';
 
 const TaskInput: React.FC = () => {
   const dispatch = useDispatch()
@@ -17,19 +18,75 @@ const TaskInput: React.FC = () => {
   }
 
   return (
-    <div className="input-form">
-      <div className="inner">
-        <input
+    <Inputform className="input-form">
+      <InputformInner className="inner">
+        <InputformInput
           type="text"
           className="input"
           value={inputTitle}
           onChange={handleInputChange}
           placeholder="please input TODO"
         />
-        <button onClick={handleSubmit} className="btn is-primary">add</button>
-      </div>
-    </div>
+        <ButtonIsPrimary onClick={handleSubmit} className="btn is-primary">add</ButtonIsPrimary>
+      </InputformInner>
+    </Inputform>
   )
 }
+
+
+const Inputform = styled.div`
+  margin-bottom: 40px;
+  background: #f9f3ee;
+  padding: 40px 0;
+  border-bottom: 1px solid #d7d2cd;
+`
+
+const Inner = styled.div`
+  max-width: 700px;
+  margin: 0 auto;
+`
+
+const InputformInner = styled(Inner)`
+  display: flex;
+  position: relative;
+`
+
+const InputformInput = styled.input`
+  width: 80%;
+  font-size: 15px;
+  outline: none;
+  border: solid 3px #d7d2cd;
+  padding: 10px;
+  border-radius: 7px;
+  margin-right: 10px;
+  &:focus {
+    background: #f9f9f0;
+  }
+`
+
+
+const Button = styled.button`
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: bold;
+  display: inline-block;
+  padding: 10px 15px;
+  text-decoration: none;
+  background: #668ad8;
+  color: #fff;
+  border: none;
+  border-bottom: solid 4px #485879;
+  border-radius: 7px;
+  outline: none;
+  &:active{
+    transform: translateY(4px);
+    border-bottom: none;
+    margin-bottom: 4px;
+  }
+`
+
+const ButtonIsPrimary = styled(Button)`
+  width: 20%;
+`
 
 export default TaskInput
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -3,6 +3,7 @@ import { Task } from '../Types'
 import { useDispatch } from 'react-redux'
 import { doneTask } from '../modules/tasksModule'
 import { deleteTask } from '../modules/tasksModule'
+import styled from 'styled-components';
 
 type Props = {
   task: Task
@@ -21,11 +22,79 @@ const TaskItem: React.FC<Props> = ({ task }) => {
         />
         <span className="checkbox-label">{ task.title }</span>
       </label>
-      <button
+      <DeleteButton
        onClick={() => dispatch(deleteTask(task))}
-       className="btn is-delete">delete</button>
+       className="btn is-delete">delete</DeleteButton>
     </li>
   )
 }
+
+/*
+const CheckboxLabel = styled.span`
+  position: relative;
+  margin-right: 30px;
+  top: -10px;
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 2px;
+    left: 0;
+    width: 18px;
+    height: 18px;
+    border: 1px solid #d7d2cd;
+    border-radius: 4px;
+  }
+`
+*/
+
+/*
+const TaskListItemInput = styled.input`
+  display: block;
+  width: 100%;
+  font-size: 15px;
+  outline: none;
+  border: solid 1px #d7d2cd;
+  padding: 7px;
+  border-radius: 4px;
+  margin-top: -6px;
+  &:focus {
+  background: #f9f9f0;
+}
+`
+*/
+
+const Button = styled.button`
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: bold;
+  display: inline-block;
+  padding: 10px 15px;
+  text-decoration: none;
+  background: #668ad8;
+  color: #fff;
+  border: none;
+  border-bottom: solid 4px #485879;
+  border-radius: 7px;
+  outline: none;
+  &:active{
+    transform: translateY(4px);
+    border-bottom: none;
+    margin-bottom: 4px;
+  }
+`
+
+const TaskListButton = styled(Button)`
+  width: 80px;
+  padding: .5em 1em .4em;
+  margin-left: 5px;
+  font-size: 13px;
+  z-index: 10;
+`
+
+const DeleteButton = styled(TaskListButton)`
+  background-color: #d86681;
+  border-bottom-color: #956270;
+`
 
 export default TaskItem
